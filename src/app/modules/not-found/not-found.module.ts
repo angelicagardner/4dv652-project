3427636966cd88615660dd769c28833b7b374ab3
Osama Zarraa <osamaz26@gmail.com>
2021-02-05 23:35:16 +0100

    styles: add angular theme feat: add not-found component

---
+++ src/app/modules/not-found/not-found.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: NotFoundComponent },
+    ]),
+  ],
+  declarations: [
+    NotFoundComponent,
+  ],
+  providers: [
+
+  ],
+  entryComponents: []
+})
+
+export class NotFoundModule {
+}
